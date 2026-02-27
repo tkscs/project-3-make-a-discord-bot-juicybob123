5e1208771a8e8ab6143f61e3309c44955b70f973
juicybob123 <okruh2028@kehillahstudent.org>
2026-02-27 11:03:18 -0800

    adv

--- my_bot.py
+++ my_bot.py
@@ -1,4 +1,5 @@
 from secret import my_username
+import random
 
 """
 **Do NOT change the name of this function.**
@@ -21,12 +22,54 @@ def should_i_respond(user_message, user_name):
 This function will be called every time the `should_i_respond` function returns `True`.
 
 * This function returns a string.
-* The bot will post the returned string on the channel where the original message was sent.
+* The bot will post the returned string on the channel where theoriginal message was sent.
 * You can have the bot respond differently to different messages and users
 """
+global dealer
+global player
+
+# def            respond(user_message, user_name):
+#   return f"""you said my name!!
+#   {user_message.replace("robot", user_name)}"""
+# cards =["1","2","3","4","5","6","7","8","9","jack","queen","king", "ace"]
+card = [1, 2, 3, 4, 5 ,6 ,7, 8, 9, 10, 10, 10, 10]
+def respond(user_message,user_name):
+  if "who is cool" in f"{user_message}":
+    return f''' yoni is cool
+    '''
+  elif "roll" in f"{user_message}":
+        return f''' {random.randint(1 , 100)}
+    '''
+  elif "sing" in f"{user_message}":
+        song = random.randint(1,2)
+        if  song == 1:
+          return f'''
+Wheels on the bus goes round and round, round and round, round and round!
+    '''
+        if song == 2:
+          return ''' Old MacDonald had a farm. E-I-E-I-O. And on that farm he had a pig. E-I-E-I-O.
+'''
+  elif "blackjack dealer" in f"{user_message,user_name}":
+    global dealer
+    dealer = user_name  
+
+    return f'''  {user_name} is now dealer 
+'''
+  elif "blackjack player" in f"{user_message,user_name}":
+    global player
+    player = user_name  
+
+    return f'''  {user_name} is now player 
+'''
+  elif "who is what" in f"{user_message,user_name}":
+
+    return f'''{dealer} is dealer and {player} is player
+     '''
 
 
-def respond(user_message, user_name):
-  return f"""you said my name!!
-  {user_message.replace("robot", user_name)}"""
-  returnf""" yoni is cool
+  
+# def respond(user_message,user_name):
+#   if "is yoni cool" in user_message:
+#    return f''' u said my name!!!
+#     {user_message.replace("robot", user_name)}'''
+   
